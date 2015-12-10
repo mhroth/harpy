7f70c27ca562a7ddbef744190cdd5e3051202305
Martin Roth <mhroth@gmail.com>
2015-12-10 15:07:01 +0000

    update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 # -DNDEBUG disables all assert checking
-clang main.c tinyosc/*.c heavy/mixer/*.c -D_GNU_SOURCE -std=c11 -Werror -Ofast -lm -lrt -lasound -lpthread -o rpistorius
+clang main.c tinyosc/*.c heavy/mixer/*.c \
+-std=c11 \
+-D_GNU_SOURCE \
+-Werror -Wno-#warnings \
+-Ofast -ffast-math \
+-mcpu=cortex-a7 -mfloat-abi=hard \
+-lm -lrt -lasound -lpthread -o rpistorius
