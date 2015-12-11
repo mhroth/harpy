0c89443ca4ee8c7f2ad49ff4511f57b0c5cd8cce
Martin Roth <mhroth@gmail.com>
2015-12-11 04:01:13 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@
 clang main.c tinyosc/*.c \
 heavy/rpis_osc/*.c \
 -std=c11 \
--D_GNU_SOURCE -DNDEBUG -DPRINT_PERF=0 -DPRINT_OSC=1 \
+-D_GNU_SOURCE -DNDEBUG -DPRINT_PERF=0 \
 -Werror -Wno-#warnings \
 -Ofast -ffast-math \
 -mcpu=cortex-a7 -mfloat-abi=hard \
--lm -lrt -lasound -lpthread -o rpistorius
+-lm -lrt -lasound -lpthread -o harpy
